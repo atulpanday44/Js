4c076d0889930484cb116498a879175b0db6e45a
Atul Panday <144517232+atulpanday44@users.noreply.github.com>
2024-08-23 22:53:33 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,25 @@
+// // // // // // for(let index=1; index<=10;index++){
+// // // // // //     console.log(index)
+// // // // // // }
+// // // // for(let i=0; i<=10;i++){
+// // // //    // console.log(`outer loop value:${i}`);
+// // // //     for(let j=0;j<=10;j++){
+// // // //         //console.log(`inner loop value  ${j} and inner loop${i}`)
+// // // //         console.log(i+'*'+ j +'= ' + i*j);
+// // // //     }
+// // // // }
+
+// // // let myArray=["thor", "superman", "wanda"]
+// // // for(let i=0; i<myArray.length;i++){
+// // //     console.log(myArray[i])
+// // // }
+    
+// // for(let i=1; i<=20; i++){
+// //     if(i==5){
+// //         break;
+
+// //     console.log(i)
+    
+// //     }
+// // }
+
