7b362b56c3e756362c571d9e7010474fd7cba17f
Atul Panday <144517232+atulpanday44@users.noreply.github.com>
2024-08-25 22:51:42 +0530

    Add files via upload

---
+++ controlFlow.js
@@ -0,0 +1,64 @@
+// // // // // // 
+// // // // // const userLoggedIn=true 
+// // // // // const debitCard=true
+// // // // // const loggedInFromGoogle= false
+// // // // // const loggedInFromEmail=true
+
+// // // // // if(userLoggedIn && debitCard){
+// // // // // console.log("You can buy it")
+// // // // // }
+
+// // // // // if(loggedInFromGoogle || loggedInFromEmail){
+// // // // //     console.log("User logged in")
+// // // // // }
+
+// // // // // SWITCH CASE in javascript
+
+// // // // switch(key){
+// // // //     case value:
+// // // //         break;
+// // // //         default:
+// // // //             bresk;
+
+// // // // }
+// // // const month=3
+// // // switch(month){
+// // //     case 1:
+// // //         console.log("January")
+// // //         break;
+// // //     case 2:
+// // //         console.log("Febr")
+// // //         break;
+// // //     case 3:
+// // //         console.log("march")
+// // //         break;
+
+// // //     case 4:
+// // //         console.log("april")
+// // //         break;
+// // //         default:
+// // //             console.log("default case match")
+// // //             break;
+// // // }
+
+// // // truthy and falsy values
+
+// // const userEmail="zts@gmail.com"
+// // if(userEmail){
+// //     console.log("Got user email"); 
+// // } else{
+// //     console.log("dont have user email")
+// // }
+
+// // Nullish Coalescing Operator(??): null undefined
+
+// let val1;
+// val1=null??10
+// var1=undefined??15
+// console.log(val1)
+
+
+// ternary operator in js
+// synatx    condition ?true:false
+const icsTeaPrice=100
+icsTeaPrice <=80 ? console.log("less than 80") : console.log("more than 80")
