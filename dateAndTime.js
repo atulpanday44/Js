65d2a402c7b2997fc6dd54f12d6e908e3d3bcac1
Atul Panday <144517232+atulpanday44@users.noreply.github.com>
2024-08-27 20:34:54 +0530

    Add files via upload

---
+++ dateAndTime.js
@@ -0,0 +1,7 @@
+// Date and Time
+let myDate=new Date()
+console.log(myDate.toDateString)
+console.log(myDate.toISOString)
+console.log(myDate.toJSON)
+console.log(myDate.toLocaleTimeString)
+// time stamp
