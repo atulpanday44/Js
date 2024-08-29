193c79630e0e3f41cfb19a5f08e45d7e63ef4457
Atul Panday <144517232+atulpanday44@users.noreply.github.com>
2024-08-29 21:18:54 +0530

    Add files via upload

---
+++ numberAndMath.js
@@ -0,0 +1,14 @@
+const score=400
+const balance=new Number(100)
+console.log(balance)
+console.log(balance.toString().length)
+
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+// maths
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(5.34))
+console.log(Math.sqrt(25))
